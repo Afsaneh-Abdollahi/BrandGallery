3021613c012b93b7a867076e1c8f9b54ab5d993b
hadiseh787 <hadisebagherian@gmail.com>
2024-03-09 20:50:38 +0330

    header edit

--- js/main.js
+++ js/main.js
@@ -2,33 +2,33 @@ $(document).ready(function () {
     $(document).ready(function () {
         "use strict";
 
-        $(".menu > ul > li:has( > ul)").addClass("menu-dropdown-icon");
+        $("header .menu > ul > li:has( > ul)").addClass("menu-dropdown-icon");
 
-        $(".menu > ul > li > ul:not(:has(ul))").addClass("normal-sub");
+        $("header .menu > ul > li > ul:not(:has(ul))").addClass("normal-sub");
 
-        $(".menu > ul").before('<a href="#" class="menu-mobile"></a>');
+        $("header .menu > ul").before('<a href="#" class="menu-mobile"></a>');
 
-        $(".menu > ul > li").hover(function (e) {
+        $("header .menu > ul > li").hover(function (e) {
             if ($(window).width() > 943) {
                 $(this).children("ul").stop(true, false).fadeToggle(150);
                 e.preventDefault();
             }
         });
 
-        $(".menu > ul > li").click(function () {
+        $("header .menu > ul > li").click(function () {
             if ($(window).width() <= 943) {
                 $(this).children("ul").fadeToggle(150);
             }
         });
 
-        $(".menu-mobile").click(function (e) {
-            $(".menu > ul").toggleClass("show-on-mobile");
+        $("header .menu-mobile").click(function (e) {
+            $("header .menu > ul").toggleClass("show-on-mobile");
             e.preventDefault();
             $(".transparent-back").addClass("active");
 
         });
         $(".show-on-mobile").click(function (e) {
-            $(".menu > ul").removeClass("show-on-mobile");
+            $("header .menu > ul").removeClass("show-on-mobile");
             e.preventDefault();
 
         });
@@ -40,14 +40,14 @@ $(document).ready(function () {
 
         }
         if (document.querySelector('.show-on-mobile')) {
-            $(".menu > ul:before").click(function () {
+            $("header .menu > ul:before").click(function () {
                 $(this).removeClass("active");
-                $(".menu.show-on-mobile").removeClass("show-on-mobile");
+                $("header .menu.show-on-mobile").removeClass("show-on-mobile");
 
             });
             $(".transparent-back.active").click(function () {
                 $(this).removeClass("active");
-                $(".menu.show-on-mobile").removeClass("show-on-mobile");
+                $("header .menu.show-on-mobile").removeClass("show-on-mobile");
 
             });
         }
