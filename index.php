3231e9fad30c93d10fe49ae67e93ab2d9799e58e
hadiseh787 <hadisebagherian@gmail.com>
2024-03-18 16:50:31 +0330

    new and most sales pr

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
       <?php
       get_template_part('partials/index', "intro");
       get_template_part('partials/index', "tab");
+      get_template_part('partials/index', "new");
+      get_template_part('partials/index', "most-sales");
       get_template_part('partials/index', "blog");
       get_template_part('partials/index', "brands");
       ?>
