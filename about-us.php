c4af894dca300dbfccdf2093d55a25574d597fc1
hadiseh787 <hadisebagherian@gmail.com>
2024-03-18 13:14:15 +0330

    about-us.php

--- about-us.php
+++ about-us.php
@@ -6,13 +6,13 @@
  */
 get_header(); ?>
 <main class="about">
-    <section class="section-top-about">
+    <section class="section-top-cover">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
                     <div class="col-lg-12">
                         <div class="banner-about ">
-                            <img src="<?php echo get_field('cover') ?>" alt="">
+                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
                             <div class="banner-about-color">
                                 <h1>درباره ما</h1>
                             </div>
--- contact-us.php
+++ contact-us.php
@@ -5,51 +5,45 @@
  * @author Afsaneh
  */
 get_header(); ?>
-
-<section class="section-top-about">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="banner-about">
-                    <img src="img/9459.png" alt="">
-                    <div class="banner-about-color">
-                        <h1>تماس با دراپا</h1>
+<main class="about">
+    <section class="section-top-cover">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="banner-about">
+                        <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+                        <div class="banner-about-color">
+                            <h1>تماس با ما</h1>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-<section class="section-top-contact">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-7 col-md-6 col-sm-12">
-                <div class="main-contact">
-                    <div class="main-contact-right">
-                        <i class="fas fa-map-marker-alt"></i>
-                        <h4>نشانی</h4>
-                        <div class="text">تهرانپارس،خیابان تیراندار،بین فلکه دوم و سوم،نبش ۱۸۶ غربی،پلاک ۳۰۸
-                        </div>
-                        <i class="fas fa-phone icon"></i>
-                        <h4>شماره تماس</h4>
-                        <div class="text">
-                            ۰۲۱-۷۶۷۰۱۰۰۸ (مدیریت داروخانه )
-                        </div>
-                        <div class="text">
-                            ۰۲۱-۷۶۷۰۲۰۰۸ (پشتیبانی سفارشات)
-                        </div>
+    </section>
+    <section class="section-top-contact">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-7 col-md-6 col-sm-12">
+                    <div class="main-contact footer footer-contact-us">
+                        <i class="fas fa-map-marker-alt"></i> <i class="fas fa-phone icon"></i>
+                        <ul>
+                            <?php while (have_rows('contact_us_repeater', 'option')) : the_row(); ?>
+                                <li> <?php echo get_sub_field('title'); ?> :
+                                    <span><?php echo get_sub_field('value'); ?></span>
+                                </li>
+                            <?php endwhile;
+                            wp_reset_query(); ?>
+                        </ul>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-5 col-md-6 col-sm-12">
-                <div class="main-contact">
-                    <div class="main-contact-left">
+                <div class="col-lg-5 col-md-6 col-sm-12">
+                    <div class="img">
                         <img src="img/map.png" alt="">
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+</main>
 <?php get_footer(); ?>
 
