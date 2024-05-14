0a184e8162667e8a247db945486443b899a83bd8
afsaneh.a <afsaneh.a@zemtrix.com>
2024-05-14 15:32:04 +0330

    edit

--- footer.php
+++ footer.php
@@ -38,7 +38,8 @@
                             $menu_obj = wp_get_nav_menu_object('footer-menu1');
                             $menu = wp_get_nav_menu_object( $menu_obj->term_id );
                             ?>
-                            <h4><?php echo get_field('footer_menu_title', $menu); ?></h4>
+<!--                            <h4>--><?php //echo get_field('footer_menu_title', $menu); ?><!--</h4>-->
+                            <h4>دسترسی آسان</h4>
                             <?php
                             wp_nav_menu(
                                 array(
@@ -58,7 +59,8 @@
                             $menu_obj = wp_get_nav_menu_object('footer-menu2');
                             $menu = wp_get_nav_menu_object( $menu_obj->term_id );
                             ?>
-                            <h4><?php echo get_field('footer_menu_title', $menu); ?></h4>
+<!--                            <h4>--><?php //echo get_field('footer_menu_title', $menu); ?><!--</h4>-->
+                            <h4>ساعت مچی</h4>
                             <?php
                             wp_nav_menu(
                                 array(
@@ -78,7 +80,8 @@
                             $menu_obj = wp_get_nav_menu_object('footer-menu3');
                             $menu = wp_get_nav_menu_object( $menu_obj->term_id );
                             ?>
-                            <h4><?php echo get_field('footer_menu_title', $menu); ?></h4>
+<!--                            <h4>--><?php //echo get_field('footer_menu_title', $menu); ?><!--</h4>-->
+                            <h4>ساعت مردانه</h4>
                             <?php
                             wp_nav_menu(
                                 array(
@@ -98,7 +101,8 @@
                             $menu_obj = wp_get_nav_menu_object('footer-menu4');
                             $menu = wp_get_nav_menu_object( $menu_obj->term_id );
                             ?>
-                            <h4><?php echo get_field('footer_menu_title', $menu); ?></h4>
+<!--                            <h4>--><?php //echo get_field('footer_menu_title', $menu); ?><!--</h4>-->
+                            <h4>ساعت زنانه</h4>
                             <?php
                             wp_nav_menu(
                                 array(
